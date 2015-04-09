8776d24a1e72387c8e58b56f293cb88caa029a79
veyselsahin <vshn@windowslive.com>
2015-04-10 00:27:39 +0300

    Create paralel.js

---
+++ paralel.js
@@ -0,0 +1,33 @@
+async.parallel([
+    function(callback){
+        setTimeout(function(){
+            callback(null, 'one');
+        }, 200);
+    },
+    function(callback){
+        setTimeout(function(){
+            callback(null, 'two');
+        }, 100);
+    }
+],
+function(err, results){
+    // sonuc dizisi bu sekilde olacak ['one','two']
+});
+
+
+// bu ornekte dizi yerine nesne kullaniliyor
+async.parallel({
+    one: function(callback){
+        setTimeout(function(){
+            callback(null, 1);
+        }, 200);
+    },
+    two: function(callback){
+        setTimeout(function(){
+            callback(null, 2);
+        }, 100);
+    }
+},
+function(err, results) {
+    // sonuc: {one: 1, two: 2}
+});
