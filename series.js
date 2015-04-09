809d80f35e026917cd7712bd265d2f3267383012
veyselsahin <vshn@windowslive.com>
2015-04-09 23:17:55 +0300

    Create series.js

---
+++ series.js
@@ -0,0 +1,15 @@
+async.series({
+    one: function(callback){
+        setTimeout(function(){
+            callback(null, 1);
+        }, 200);
+    },
+    two: function(callback){
+        setTimeout(function(){
+            callback(null, 2);
+        }, 100);
+    }
+},
+function(err, results) {
+    //sonuc {one: 1, two: 2}
+});
